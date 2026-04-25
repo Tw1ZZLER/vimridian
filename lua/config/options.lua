0e239c6421e96db71148b9857616884d41d8cc96
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2026-04-24 20:44:18 -0400

    Treesitter things

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -7,3 +7,9 @@ vim.opt.relativenumber = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.expandtab = true
+
+-- Treesitter folding
+vim.opt.foldenable = true
+vim.opt.foldlevel = 99
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
