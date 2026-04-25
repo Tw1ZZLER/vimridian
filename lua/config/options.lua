d64767e5b039de3c5fcbcb9961b3136d3bdacd66
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2026-04-24 20:32:51 -0400

    Basic options

---
+++ lua/config/options.lua
@@ -0,0 +1,9 @@
+-- Line numbers, cursorline, relative line numbers
+vim.opt.number = true
+vim.opt.cursorline = true
+vim.opt.relativenumber = true
+
+-- Tab spacing should be 4
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
