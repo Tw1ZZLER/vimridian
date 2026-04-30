d8ec8676adac5dfd686dd39d5ae1777c69a7a907
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2026-04-30 12:20:44 -0400

    Conform.nvim added with formatters for lua and Nix

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,31 @@
+-- Formatting plugin
+-- credit: https://www.lazyvim.org/plugins/formatting#conformnvim
+-- credit: https://github.com/stevearc/conform.nvim#installation
+return {
+	"conform.nvim",
+	after = function()
+		require("conform").setup({
+			default_format_opts = {
+				timeout_ms = 3000,
+				async = false, -- not recommended to change
+				quiet = false, -- not recommended to change
+				lsp_format = "fallback", -- not recommended to change
+			},
+			format_on_save = {
+				-- These options will be passed to conform.format()
+				timeout_ms = 500,
+				lsp_format = "fallback",
+			},
+			formatters_by_ft = {
+				lua = { "stylua" },
+				nix = { "nixfmt" },
+				-- Conform will run multiple formatters sequentially
+				-- python = { "isort", "black" },
+				-- You can customize some of the format options for the filetype (:help conform.format)
+				-- rust = { "rustfmt", lsp_format = "fallback" },
+				-- Conform will run the first available formatter
+				-- javascript = { "prettierd", "prettier", stop_after_first = true },
+			},
+		})
+	end,
+}
