8f9d45cd8d9f22adb403d2a46db289041c9ba7f5
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2026-05-14 00:08:09 -0400

    oil load mini.icons

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,32 +1,35 @@
 -- Single-directory, filesystem as a buffer, file explorer
 return {
-	"oil.nvim",
-	-- Disable lazy-loading, otherwise won't take over as default file explorer
-	lazy = false,
-	keys = {
-		{
-			"-",
-			"<CMD>Oil<CR>",
-			mode = "n",
-			desc = "Open parent directory",
-		},
-		{
-			"<leader>e",
-			"<CMD>Oil --float<CR>",
-			mode = "n",
-			desc = "Oil (float)",
-		},
-	},
-	after = function()
-		require("oil").setup({
-			default_file_explorer = true,
-			columns = {
-				"icon",
-				-- "permissions",
-				-- "size",
-				-- "mtime",
-			},
-			watch_for_changes = true,
-		})
-	end,
+    "oil.nvim",
+    -- Disable lazy-loading, otherwise won't take over as default file explorer
+    lazy = false,
+    keys = {
+        {
+            "-",
+            "<CMD>Oil<CR>",
+            mode = "n",
+            desc = "Open parent directory",
+        },
+        {
+            "<leader>e",
+            "<CMD>Oil --float<CR>",
+            mode = "n",
+            desc = "Oil (float)",
+        },
+    },
+    before = function()
+        LZN.trigger_load("mini.icons")
+    end,
+    after = function()
+        require("oil").setup({
+            default_file_explorer = true,
+            columns = {
+                "icon",
+                -- "permissions",
+                -- "size",
+                -- "mtime",
+            },
+            watch_for_changes = true,
+        })
+    end,
 }
