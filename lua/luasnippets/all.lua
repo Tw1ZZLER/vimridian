db5ef98861f01591912409ef717bbb4fbb6a643b
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2026-05-14 00:06:06 -0400

    added some extra stuff to luasnip

--- lua/luasnippets/all.lua
+++ lua/luasnippets/all.lua
@@ -13,18 +13,22 @@ M.f = ls.function_node;
 M.c = ls.choice_node;
 M.d = ls.dynamic_node;
 M.r = ls.restore_node;
+M.ms = ls.multi_snippet
 M.events = require("luasnip.util.events");
 M.ai = require("luasnip.nodes.absolute_indexer");
 M.fmt = require("luasnip.extras.fmt").fmt;
-M.lambda = require("luasnip.extras").l;
+M.fmta = require("luasnip.extras.fmt").fmta;
+M.l = require("luasnip.extras").lambda;
+M.dl = require("luasnip.extras").dynamic_lambda;
+M.conds = require("luasnip.extras.expand_conditions");
 M.rep = require("luasnip.extras").rep;
 M.p = require("luasnip.extras").partial;
 M.m = require("luasnip.extras").match;
 M.n = require("luasnip.extras").nonempty;
-M.dl = require("luasnip.extras").dynamic_lambda;
-M.fmta = require("luasnip.extras.fmt").fmta;
 M.types = require("luasnip.util.types");
-M.conds = require("luasnip.extras.expand_conditions");
+M.postfix = require("luasnip.extras.postfix").postfix
+M.parse = require("luasnip.util.parser").parse_snippet
+M.autosnippet = ls.extend_decorator.apply(ls.snippet, { snippetType = "autosnippet" })
 M.newline = function() return ls.text_node({"", ""}) end;
 
 return M
