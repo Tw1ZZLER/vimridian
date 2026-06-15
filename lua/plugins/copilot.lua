6cd150b819655381b6e93e9ebaab6e10e4ead98b
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2026-06-15 12:39:37 -0500

    Copilot config

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,8 @@
+-- Copilot LSP provider and stuff
+return {
+    "copilot-lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    before = function() end,
+    after = function() require("copilot").setup {} end,
+}
